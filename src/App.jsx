26c55612a386e5e89a106ff2ee050c9bd8ca595b
Alex <ajcbrown820@gmail.com>
2017-11-24 12:25:06 +1030

    Import react-tippy default CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Provider } from "mobx-react";
 import RouterContainer from "./RouterContainer.jsx";
 import { UIStore } from "./mobX/stores/UIStore";
+import "react-tippy/dist/tippy.css";
 require("./globalStyles");
 
 const App = () => (
