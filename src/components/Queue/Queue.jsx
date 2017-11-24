aa7787b9718ef4db6e86ffe6c61c5ef980e6777e
Alex <ajcbrown820@gmail.com>
2017-11-24 12:26:27 +1030

    Move Queue render logic to a class method

--- src/components/Queue/Queue.jsx
+++ src/components/Queue/Queue.jsx
@@ -12,45 +12,40 @@ class Queue extends React.Component {
     this.queueNode = ref;
   };
 
-  render() {
-    let { label, global } = this.props;
+  renderTasks = () => {
+    let { global } = this.props;
     let currentState = global
-      ? this.props.UIStore.globalTaskQueueStore.currentTasks
-      : this.props.UIStore.microTaskQueueStore.currentTasks;
+      ? this.props.globalTasks ? this.props.globalTasks : null
+      : this.props.microTasks ? this.props.microTasks : null;
+    //let currentTasks = currentState.tasks ? currentState.tasks : null;
 
+    if (currentState) {
+      currentState.tasks.map((task, index) => (
+        <Transition key={task + index} appear timeout={2000}>
+          {mountState => (
+            <MicroTask
+              active={index === currentState.tasks.length - 1}
+              setHoveredFrame={this.props.UIStore.setHoveredFrame}
+              setActiveTab={this.props.UIStore.codeEditorStore.setActiveTab}
+              scriptName={task.scriptName}
+              lines={task.lines}
+              mountState={mountState}
+              animation={currentState.animation}
+              key={task}
+            />
+          )}
+        </Transition>
+      ));
+    }
+  };
+
+  render() {
+    const { label } = this.props;
     return (
       <div className={container}>
         <QueuLabel>{label}</QueuLabel>
         <div className={queueStyle} ref={this.bindQueueNode}>
-          {
-            do {
-              if (currentState) {
-                if (currentState.tasks) {
-                  currentState.tasks.map((task, index) => (
-                    <Transition key={task + index} appear timeout={2000}>
-                      {mountState => (
-                        <MicroTask
-                          active={index == currentState.tasks.length - 1}
-                          setHoveredFrame={this.props.UIStore.setHoveredFrame}
-                          setActiveTab={
-                            this.props.UIStore.codeEditorStore.setActiveTab
-                          }
-                          scriptName={task.scriptName}
-                          lines={task.lines}
-                          mountState={mountState}
-                          animation={currentState.animation}
-                          key={task}
-                        />
-                      )}
-                    </Transition>
-                  ));
-                }
-              } else {
-                null;
-              }
-            }
-          }
-          }}
+          {this.renderTasks()}
         </div>
       </div>
     );
@@ -59,8 +54,9 @@ class Queue extends React.Component {
 
 // FIX THIS
 export default inject(stores => ({
-    globalTasks: stores.UIStore
-})(Queue);
+  globalTasks: stores.UIStore.globalTaskQueueStore.currentTasks,
+  microTasks: stores.UIStore.microTaskQueueStore.currentTasks
+}))(Queue);
 
 const QueuLabel = styled("div")`
   color: rgb(200, 200, 200);
