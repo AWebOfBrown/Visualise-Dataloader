b5241f13461002cf8c720582ec2947d4752e3d92
Alex <ajcbrown820@gmail.com>
2017-12-15 23:06:56 +1030

    Add context click to microtask

--- src/components/Queue/MicroTask/MicroTask.jsx
+++ src/components/Queue/MicroTask/MicroTask.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { css } from "emotion";
+import provideTooltip from "../MicrotaskTooltip";
 
-const MicroTask = ({ onClick, onMouseEnter, onMouseLeave, innerRef }) => {
+const MicroTask = ({
+  onClick,
+  onContextMenu,
+  onMouseEnter,
+  onMouseLeave,
+  innerRef
+}) => {
   return (
     <div
       className={Task}
       onClick={onClick}
       onMouseEnter={onMouseEnter}
       onMouseLeave={onMouseLeave}
+      onContextMenu={onContextMenu}
       ref={innerRef}
     />
   );
 };
 
-export default MicroTask;
+export default provideTooltip(MicroTask);
 
 const Task = css`
   cursor: pointer;
-  marginleft: 20px;
-  backgroundcolor: #0f69f9;
+  margin-left: 20px;
+  background-color: #0f69f9;
   width: 40px;
   height: 20px;
-  boxshadow: 0px 4px 20px 0px rgba(29, 79, 159, 1);
-  opacity: 0;
+  box-shadow: 0px 4px 20px 0px rgba(29, 79, 159, 1);
+  opacity: 1;
 `;
