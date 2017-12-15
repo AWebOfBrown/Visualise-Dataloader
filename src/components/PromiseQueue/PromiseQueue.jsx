e4ca9fe3ac1e1f277f50360031c3604dfc0aa8c6
Alex <ajcbrown820@gmail.com>
2017-12-15 23:05:49 +1030

    Fix promise queue styling

--- src/components/PromiseQueue/PromiseQueue.jsx
+++ src/components/PromiseQueue/PromiseQueue.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Transition from "react-transition-group/Transition";
 import TransitionGroup from "react-transition-group/TransitionGroup";
-import { css } from "emotion";
+import styled, { css } from "react-emotion";
 import { observer, inject } from "mobx-react";
 
 import Tuple from "./Tuple";
@@ -24,10 +24,22 @@ class PromiseQueue extends React.Component {
   render() {
     return (
       <div className={promiseQueueStyle}>
-        <div className={label}> Promise Queue (Array)</div>
-        <TransitionGroup>
-          <div>{this.renderPromises()}</div>
-        </TransitionGroup>
+        <span className={label}> Promise Queue (Array)</span>
+        <div
+          style={{
+            display: "flex",
+            width: "100%",
+            height: "100px",
+            justifyContent: "space-around",
+            alignItems: "center"
+          }}
+        >
+          <Bracket>{"["} </Bracket>
+          <TransitionGroup>
+            <div>{this.renderPromises()}</div>
+          </TransitionGroup>
+          <Bracket>{"]"}</Bracket>
+        </div>
         <span className={label}>Property set at DataLoader.js line 48</span>
       </div>
     );
@@ -39,13 +51,15 @@ export default inject(stores => ({
 }))(observer(PromiseQueue));
 
 const promiseQueueStyle = css`
+  color: black;
   flex-flow: column nowrap;
-  justify-content: space-between;
   align-items: center;
   width: 100%;
-  max-width: 500px;
-  height: 100px;
-  background-color: black;
 `;
 
-const label = css`color: white;`;
+const Bracket = styled("span")`font-size: 30px;`;
+
+const label = css`
+  color: black;
+  margin: 0 auto;
+`;
